7cd53251cc268ddc318af230703541a0c882ede8
David Donges <david.donges@mni.thm.de>
2020-06-04 12:03:38 +0200

    Publish incoming orders to order queue

--- src/main/java/de/thm/mni/vs/gruppe5/hq/Headquarter.java
+++ src/main/java/de/thm/mni/vs/gruppe5/hq/Headquarter.java
@@ -3,6 +3,7 @@ package de.thm.mni.vs.gruppe5.hq;
 import de.thm.mni.vs.gruppe5.common.Config;
 import de.thm.mni.vs.gruppe5.common.Publisher;
 import de.thm.mni.vs.gruppe5.common.Subscriber;
+import de.thm.mni.vs.gruppe5.common.model.*;
 
 import javax.jms.JMSException;
 import javax.jms.MessageListener;
@@ -22,10 +23,28 @@ public class Headquarter {
     }
 
     private void setup() throws JMSException {
-        var incomingOrders = new Subscriber(Config.INCOMING_ORDER_QUEUE, messageListener);
+        var incomingOrders = new Subscriber(Config.INCOMING_ORDER_QUEUE, incomingOrderListener);
         var finishedOrders = new Subscriber(Config.FINISHED_ORDER_QUEUE, messageListener);
         orderPublisher = new Publisher(Config.ORDER_QUEUE);
     }
+
+    private void processIncomingOrder(FridgeOrder order) throws JMSException {
+        System.out.println("Send order to factories: " + order.toString());
+        orderPublisher.publish(order);
+    }
+
+    private MessageListener incomingOrderListener = m -> {
+        try {
+            var objectMessage = (ObjectMessage) m;
+            var order = (FridgeOrder) objectMessage.getObject();
+
+            System.out.println("Received order: " + order.toString());
+
+            processIncomingOrder(order);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    };
 
     private MessageListener messageListener = m -> {
         if (m instanceof ObjectMessage) {
