24a33a25ff3e330faacc2c1b2581b5d1dbd02976
Elias Kupferschmitt <elias.kupferschmitt@mni.thm.de>
2020-06-04 12:49:02 +0200

    Add production simulation

--- src/main/java/de/thm/mni/vs/gruppe5/factory/Production.java
+++ src/main/java/de/thm/mni/vs/gruppe5/factory/Production.java
@@ -1,4 +1,56 @@
 package de.thm.mni.vs.gruppe5.factory;
 
-public class Production {
+import de.thm.mni.vs.gruppe5.common.model.FridgeOrder;
+import de.thm.mni.vs.gruppe5.common.model.OrderItem;
+import de.thm.mni.vs.gruppe5.common.model.OrderStatus;
+
+import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+public class Production implements IProduction {
+    private void waitRandom(int maxSeconds) {
+        var r = new Random();
+        try {
+            Thread.sleep(r.nextInt(maxSeconds) * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Future<FridgeOrder> orderParts(FridgeOrder order) {
+        return CompletableFuture.supplyAsync(() -> {
+            waitRandom(10);
+            return order;
+        });
+    }
+
+    @Override
+    public Future<FridgeOrder> produce(FridgeOrder order) {
+        return CompletableFuture.supplyAsync(() -> {
+            waitRandom(10);
+            return order;
+        });
+    }
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        var order = new FridgeOrder("Test", Set.of(new OrderItem(null, 123), new OrderItem(null, 456)), OrderStatus.RECEIVED, false);
+        var production = new Production();
+        System.out.println("Ordering parts");
+        var partOrderFuture = production.orderParts(order);
+        order.setPartsOrdered(true);
+        System.out.println("Waiting for parts...");
+        partOrderFuture.get();
+        System.out.println("Parts received");
+        order.setStatus(OrderStatus.PRODUCING);
+        System.out.println("Starting production");
+        var productionFuture = production.produce(order);
+        System.out.println("Waiting for production");
+        productionFuture.get();
+        order.setStatus(OrderStatus.COMPLETED);
+        System.out.println("Order completed");
+    }
 }
