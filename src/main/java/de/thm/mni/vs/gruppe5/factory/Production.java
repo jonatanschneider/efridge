2c4895d928fe18d6b3dd7b5970871303b6449a29
Elias Kupferschmitt <elias.kupferschmitt@mni.thm.de>
2020-06-04 13:13:45 +0200

    Remove test main

--- src/main/java/de/thm/mni/vs/gruppe5/factory/Production.java
+++ src/main/java/de/thm/mni/vs/gruppe5/factory/Production.java
@@ -35,22 +35,4 @@ public class Production implements IProduction {
             return order;
         });
     }
-
-    public static void main(String[] args) throws InterruptedException, ExecutionException {
-        var order = new FridgeOrder("Test", Set.of(new OrderItem(null, 123), new OrderItem(null, 456)), OrderStatus.RECEIVED, false);
-        var production = new Production();
-        System.out.println("Ordering parts");
-        var partOrderFuture = production.orderParts(order);
-        order.setPartsOrdered(true);
-        System.out.println("Waiting for parts...");
-        partOrderFuture.get();
-        System.out.println("Parts received");
-        order.setStatus(OrderStatus.PRODUCING);
-        System.out.println("Starting production");
-        var productionFuture = production.produce(order);
-        System.out.println("Waiting for production");
-        productionFuture.get();
-        order.setStatus(OrderStatus.COMPLETED);
-        System.out.println("Order completed");
-    }
 }
