866337aa6871f53239498c54adf5c87b013628db
David Donges <david.donges@mni.thm.de>
2020-06-04 12:10:53 +0200

    Remove test code

--- src/main/java/de/thm/mni/vs/gruppe5/factory/Factory.java
+++ src/main/java/de/thm/mni/vs/gruppe5/factory/Factory.java
@@ -8,25 +8,5 @@ import java.util.HashSet;
 
 public class Factory {
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("eFridge");
-        var em = emf.createEntityManager();
-        var part = new Part(2.4, Supplier.CoolMechanics);
-
-        var set = new HashSet<ProductPart>();
-        var productPart = new ProductPart(part, 2);
-        set.add(productPart);
-        var product = new Product("Tolles Produkt", 4, set);
-
-        var set2 = new HashSet<OrderItem>();
-        var item = new OrderItem(product, 2);
-        set2.add(item);
-        var fridgeOrder = new FridgeOrder("customerId", set2, OrderStatus.RECEIVED, true);
-
-
-        em.getTransaction().begin();
-        em.persist(fridgeOrder);
-        em.getTransaction().commit();
-        em.close();
-        emf.close();
     }
 }
