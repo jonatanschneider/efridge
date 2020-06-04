c49fd862a10a955ea8b452b17786c82f7ac36be1
David Donges <david.donges@mni.thm.de>
2020-06-04 12:11:17 +0200

    Listen to order queue

--- src/main/java/de/thm/mni/vs/gruppe5/factory/Factory.java
+++ src/main/java/de/thm/mni/vs/gruppe5/factory/Factory.java
@@ -1,12 +1,43 @@
 package de.thm.mni.vs.gruppe5.factory;
 
+import de.thm.mni.vs.gruppe5.common.Config;
+import de.thm.mni.vs.gruppe5.common.Subscriber;
 import de.thm.mni.vs.gruppe5.common.model.*;
 
+import javax.jms.JMSException;
+import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.util.HashSet;
 
 public class Factory {
+    private IProduction production;
+
     public static void main(String[] args) {
+        var factory = new Factory();
+
+        try {
+            factory.setup();
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
     }
+
+    private void setup() throws JMSException {
+        var orders = new Subscriber(Config.ORDER_QUEUE, processOrder);
+        // TODO setup production
+    }
+
+    private final MessageListener processOrder = m -> {
+        try {
+            var objectMessage = (ObjectMessage) m;
+            var order = (FridgeOrder) objectMessage.getObject();
+
+            System.out.println("Received order: " + order.toString());
+            // TODO: Trigger production when setup
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    };
 }
