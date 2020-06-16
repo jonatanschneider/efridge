f7abfcf7d50f1a82e4e38e44e99ed3ac3e5be512
Jonatan Schneider <j.schneider1004@gmail.com>
2020-06-16 10:53:03 +0200

    Add JPA Annotations

--- src/main/java/de/thm/mni/vs/gruppe5/common/model/SupportTicket.java
+++ src/main/java/de/thm/mni/vs/gruppe5/common/model/SupportTicket.java
@@ -1,17 +1,79 @@
 package de.thm.mni.vs.gruppe5.common.model;
 
-import org.eclipse.persistence.jpa.jpql.parser.DateTime;
+import javax.persistence.*;
+import java.util.Date;
 
+@Entity
 public class SupportTicket {
+    @Id
+    @GeneratedValue
     private String id;
 
     private String customerId;
 
     private boolean isClosed;
 
-    private DateTime creationTime;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date creationTime;
 
-    private DateTime closingTime;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date closingTime;
 
     private String text;
+
+    public SupportTicket() {
+
+    }
+
+    public SupportTicket(String customerId, boolean isClosed, Date creationTime, Date closingTime, String text) {
+        this.customerId = customerId;
+        this.isClosed = isClosed;
+        this.creationTime = creationTime;
+        this.closingTime = closingTime;
+        this.text = text;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public boolean isClosed() {
+        return isClosed;
+    }
+
+    public void setClosed(boolean closed) {
+        isClosed = closed;
+    }
+
+    public Date getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(Date creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    public Date getClosingTime() {
+        return closingTime;
+    }
+
+    public void setClosingTime(Date closingTime) {
+        this.closingTime = closingTime;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
