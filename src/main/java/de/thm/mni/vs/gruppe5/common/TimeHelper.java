6d71c2ef9932b65d488c02df383306ccb2a406f8
David Donges <david.donges@mni.thm.de>
2020-06-18 23:48:30 +0200

    Move wait operations to helper class

---
+++ src/main/java/de/thm/mni/vs/gruppe5/common/TimeHelper.java
@@ -0,0 +1,20 @@
+package de.thm.mni.vs.gruppe5.common;
+
+import java.util.Random;
+
+public class TimeHelper {
+    public static void waitRandom(int maxSeconds) {
+        var r = new Random();
+        var seconds = r.nextInt(maxSeconds);
+        waitTime(seconds);
+    }
+
+    public static void waitTime(long seconds) {
+        try {
+            System.out.println("Waiting " + seconds + " seconds");
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
