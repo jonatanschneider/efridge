476d142f77a47a8ac2efafc81a5c3c5e9be7b0b6
Jonatan Schneider <j.schneider1004@gmail.com>
2020-06-26 15:29:56 +0200

    Persist SupportTicket in SupportCenter

--- src/main/java/de/thm/mni/vs/gruppe5/support/SupportCenter.java
+++ src/main/java/de/thm/mni/vs/gruppe5/support/SupportCenter.java
@@ -5,21 +5,25 @@ import de.thm.mni.vs.gruppe5.common.Location;
 import de.thm.mni.vs.gruppe5.common.Publisher;
 import de.thm.mni.vs.gruppe5.common.Subscriber;
 import de.thm.mni.vs.gruppe5.common.model.SupportTicket;
+import de.thm.mni.vs.gruppe5.util.DatabaseUtility;
 
 import javax.jms.JMSException;
 import javax.jms.MessageListener;
 import javax.jms.ObjectMessage;
+import javax.persistence.EntityManagerFactory;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class SupportCenter {
-    private int agents;
     private final Location location;
+    private int agents;
     private List<SupportTicket> currentTickets;
     private Publisher finishedTicketPublisher;
     private IAgent agent;
     private Subscriber ticketSubscriber;
+    private EntityManagerFactory emf;
+
 
     public static void main(String[] args) {
         Location location;
@@ -34,36 +38,23 @@ public class SupportCenter {
             return;
         }
 
-        var supportCenter = new SupportCenter(location, agents);
-
         try {
-            supportCenter.setup();
+            var supportCenter = new SupportCenter(location, agents);
+            Runtime.getRuntime().addShutdownHook(supportCenter.closeResources());
+
         } catch (JMSException e) {
             e.printStackTrace();
         }
     }
 
-    public SupportCenter(Location location, int agents) {
+    public SupportCenter(Location location, int agents) throws JMSException {
+        this.emf = DatabaseUtility.getEntityManager(location);
         this.location = location;
         this.agents = agents;
         this.currentTickets = Collections.synchronizedList(new ArrayList<>(agents));
-    }
-
-    private void setup() throws JMSException {
         ticketSubscriber = new Subscriber(Config.TICKET_QUEUE, processTicket);
         finishedTicketPublisher = new Publisher(Config.FINISHED_TICKET_QUEUE);
         agent = new Agent();
-    }
-
-    private void reportFinishedTicket(SupportTicket ticket) {
-        System.out.println("Finished ticket " + ticket.toString());
-        try {
-            finishedTicketPublisher.publish(ticket);
-            currentTickets.remove(ticket);
-            ticketSubscriber.restart();
-        } catch (JMSException e) {
-            e.printStackTrace();
-        }
     }
 
     private final MessageListener processTicket = m -> {
@@ -72,6 +63,7 @@ public class SupportCenter {
             var ticket = (SupportTicket) objectMessage.getObject();
 
             System.out.println("Received ticket: " + ticket.toString());
+            DatabaseUtility.persist(emf, ticket);
             if (currentTickets.size() < agents) {
                 if (currentTickets.size() == agents - 1) {
                     ticketSubscriber.pause();
@@ -87,4 +79,32 @@ public class SupportCenter {
             e.printStackTrace();
         }
     };
+
+    private void reportFinishedTicket(SupportTicket ticket) {
+        System.out.println("Finished ticket " + ticket.toString());
+        try {
+            DatabaseUtility.merge(emf, ticket);
+            finishedTicketPublisher.publish(ticket);
+            currentTickets.remove(ticket);
+            ticketSubscriber.restart();
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Thread closeResources() {
+        return new Thread(() -> {
+            System.out.println("Shutdown headquarter");
+            System.out.println("Closing database connection");
+            emf.close();
+            System.out.println("Closing ActiveMQ connections");
+            if (ticketSubscriber != null) {
+                ticketSubscriber.close();
+            }
+            if (finishedTicketPublisher != null) {
+                finishedTicketPublisher.close();
+            }
+
+        });
+    }
 }
