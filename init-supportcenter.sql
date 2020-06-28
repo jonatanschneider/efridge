de38628eb1cb5350b36adb71e9db4938be501640
Elias Kupferschmitt <elias.kupferschmitt@mni.thm.de>
2020-06-28 19:01:11 +0200

    completedAt timestamp for SupportTicket

--- init-supportcenter.sql
+++ init-supportcenter.sql
@@ -1,1 +1,1 @@
-create table SupportTicket (id varchar(255) not null, closingTime timestamp, creationTime timestamp, customerId varchar(255), isClosed boolean not null, text varchar(5000), primary key (id));
+create table SupportTicket (id varchar(255) not null, closingTime timestamp, creationTime timestamp, customerId varchar(255), isClosed boolean not null, text varchar(5000), completedAt timestamp, primary key (id));
