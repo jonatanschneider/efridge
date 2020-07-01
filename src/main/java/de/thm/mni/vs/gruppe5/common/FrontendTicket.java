e4ec4f93fd7bb41809a23258dec8d2a4811f05ee
Elias Kupferschmitt <elias.kupferschmitt@mni.thm.de>
2020-07-01 15:25:52 +0200

    JavaDoc for FrontendTicket

--- src/main/java/de/thm/mni/vs/gruppe5/common/FrontendTicket.java
+++ src/main/java/de/thm/mni/vs/gruppe5/common/FrontendTicket.java
@@ -1,10 +1,12 @@
 package de.thm.mni.vs.gruppe5.common;
 
 import de.thm.mni.vs.gruppe5.common.model.TicketStatus;
-
 import java.util.Date;
 import java.util.Scanner;
 
+/**
+ * Representation of support tickets used to send newly created tickets from CLI to HQ
+ */
 public class FrontendTicket implements FrontendItem {
     public String customerId;
     public Date creationTime;
@@ -12,12 +14,20 @@ public class FrontendTicket implements FrontendItem {
     public Date closingTime;
     public String text;
 
+    /**
+     * Validate tickets
+     * @return whether or not a ticket is valid
+     */
     @Override
     public boolean isValid() {
         if (customerId == null || customerId.isBlank() || creationTime == null || text == null || text.isBlank()) return false;
         return closingTime == null || !closingTime.before(creationTime);
     }
 
+    /**
+     * Allows interactive creation of a new ticket via console
+     * @return open ticket with user-defined texts and current date as opening date
+     */
     @Override
     public FrontendItem interactiveCreation() {
         var scanner = new Scanner(System.in);
