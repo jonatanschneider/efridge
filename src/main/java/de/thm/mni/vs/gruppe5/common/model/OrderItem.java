b72378706fcbba9e6741be5b9735c0e98388aa10
Elias Kupferschmitt <elias.kupferschmitt@mni.thm.de>
2020-07-08 14:36:14 +0200

    Add JavaDoc

--- src/main/java/de/thm/mni/vs/gruppe5/common/model/OrderItem.java
+++ src/main/java/de/thm/mni/vs/gruppe5/common/model/OrderItem.java
@@ -62,21 +62,39 @@ public class OrderItem implements Serializable, Completable {
                 '}';
     }
 
+    /**
+     * Initialise waiting time by storing a Date in the future
+     *
+     * @param seconds waiting time in seconds
+     */
     @Override
     public void init(int seconds) {
         completedAt = new Date(System.currentTimeMillis() + seconds * 1000);
     }
 
+    /**
+     * Initialise waiting time by storing a Date in the future. Pick a random waiting time from 1 to parameter.
+     *
+     * @param seconds maximum waiting time in seconds
+     */
     @Override
     public void initRandom(int seconds) {
         init(new Random().nextInt(seconds) + 1);
     }
 
+    /**
+     * @return whether or not a completion date has been set
+     */
     @Override
     public boolean hasInit() {
         return completedAt != null;
     }
 
+    /**
+     * Uses Thread.sleep to wait until completion date is in the past.
+     *
+     * @throws InterruptedException in case of manual interruption
+     */
     @Override
     public void complete() throws InterruptedException {
         while (completedAt.after(new Date(System.currentTimeMillis())))
